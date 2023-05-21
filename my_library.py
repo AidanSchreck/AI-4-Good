dfb564e294d5011e0e0fb90b1b15865b855d7d50
AidanSchreck <130413108+AidanSchreck@users.noreply.github.com>
2023-05-21 16:36:54 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -10,11 +10,11 @@ def test_it():
   return 'loaded'
 
 
-def cond_prob(table, evidence, evidence_val, target1, target_val):
-  t_subset = up_table_subset(table, target1, 'equals', target_val)
+def cond_prob(table, evidence, evidence_value, target, target_value):
+  t_subset = up_table_subset(table, target, 'equals', target_value)
   e_list = up_get_column(t_subset, evidence)
-  p_b_a = sum([1 if v==evidence_val else 0 for v in e_list])/len(e_list)
-  return p_b_a
+  p_b_a = sum([1 if v==evidence_value else 0 for v in e_list])/len(e_list)
+  return p_b_a + .01
 
 def cond_probs_product(table, evidence_val, target1, target_val):
   cond_prob_list = []
