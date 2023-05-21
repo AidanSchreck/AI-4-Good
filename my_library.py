55f5e38bd50dc653ab37ee07fe8e4e4a067b72c0
AidanSchreck <130413108+AidanSchreck@users.noreply.github.com>
2023-05-21 16:42:51 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -10,31 +10,31 @@ def test_it():
   return 'loaded'
 
 
-def cond_prob(table, evidence, evidence_value, target, target_value):
-  t_subset = up_table_subset(table, target, 'equals', target_value)
+def cond_prob(table, evidence, evidence_val, target, target_val):
+  t_subset = up_table_subset(table, target, 'equals', target_val)
   e_list = up_get_column(t_subset, evidence)
-  p_b_a = sum([1 if v==evidence_value else 0 for v in e_list])/len(e_list)
+  p_b_a = sum([1 if v==evidence_val else 0 for v in e_list])/len(e_list)
   return p_b_a + .01
 
-def cond_probs_product(table, evidence_val, target1, target_val):
+def cond_probs_product(table, evidence_val, target, target_val):
   cond_prob_list = []
   table_columns = up_list_column_names(table)
   evidence_columns = table_columns[:-1]
   evidence_complete = up_zip_lists(evidence_columns, evidence_val)
   for a, b in evidence_complete:
-    prob = cond_prob(table, a, b, target_column, target_val)
+    prob = cond_prob(table, a, b, target, target_val)
     cond_prob_list += [prob]
   partial_numerator = up_product(cond_prob_list)
   return partial_numerator
 
-def prior_prob (table, target1, target_val):
-  t_list = up_get_column(table, target1)
+def prior_prob (table, target, target_val):
+  t_list = up_get_column(table, target)
   p_a = sum([1 if v==target_val else 0 for v in t_list])/len(t_list)
   return p_a
 
-def naive_bayes(table, evidence_row, target1):
-  neg = cond_probs_product(table, evidence_row, target1, 0) * prior_prob(table, target1, 0)
-  pos = cond_probs_product(table, evidence_row, target1, 1) * prior_prob(table, target1, 1)
+def naive_bayes(table, evidence_row, target):
+  neg = cond_probs_product(table, evidence_row, target, 0) * prior_prob(table, target, 0)
+  pos = cond_probs_product(table, evidence_row, target, 1) * prior_prob(table, target, 1)
   neg,pos = compute_probs(neg, pos)
   return [neg,pos]
 
